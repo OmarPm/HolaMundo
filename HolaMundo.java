bcc14602af5992715520eccc529c198ef6e57cbb
Omar <omarposadas288@gmail.com>
2018-01-03 12:56:37 -0600

    Colores definidos

--- HolaMundo.java
+++ HolaMundo.java
@@ -4,6 +4,8 @@
 
 public class HolaMundo{
 	public  static void Main(String[] args){
+		String naranja = "\033[33m";
+		String azul = "\033[34m";
 		int a = 5;
 		int b = 3;
 		System.out.println("Hola HolaMundo");
